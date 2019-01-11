56500be2d4a3f69eaf6792e1897c947cc34ab450
Brandon Brasson <rocksong622@gmail.com>
2019-01-11 11:26:28 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,11 +16,10 @@ require 'pry'
 
 
   def remove_strawberry(contacts)
-    if attribute == :favorite_ice_cream_flavors
-      data.delete_if{|ice_cream| ice_cream == "strawberry"}
+  if attribute == :favorite_ice_cream_flavors
+  data.delete_if{|ice_cream| ice_cream == "strawberry"}
   contacts.each do |person, contact_details_hash|
   contact_details_hash.each do |attribute, data|
-
   end
   end
   end
